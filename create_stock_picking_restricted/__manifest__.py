9275cf27108f37323a1e9697e48b44f17dc5a25e
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-09-09 18:48:11 +0300

    Add files via upload

---
+++ create_stock_picking_restricted/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Create Manual Stock Picking Restricted',
+    'version': '6.1.0',
+    'category': 'Warehouse',
+    "author": 'Zero Systems',
+    "company": 'Zero for Information Systems',
+    "website": "https://www.erpzero.com",
+    "email": "sales@erpzero.com",
+    "sequence": 0,
+    'license': 'OPL-1',
+    'live_test_url': 'https://youtu.be/zvtd_wkRdqg',
+    'summary': 'Receipt, Delivery and Internal Picking Transfers Manual Creation Restricted',
+    "description": 'Receipt, Delivery and Internal Picking Transfers Manual Creation Restricted',
+    'depends': ['base','stock'],
+    'data': [
+        'security/security.xml',
+
+    ],
+    "price": 25.00,
+    "currency": 'EUR',
+    'installable': True,
+    'auto_install': False,
+    "application": True,
+    'images': ['static/description/stock_picking.png'],
+    'pre_init_check_vers': 'pre_init_check_vers',
+}
+
