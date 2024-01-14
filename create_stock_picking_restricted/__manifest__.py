dd3838e26b35730305b7d1040aa92562afa5897f
Zero Systems <isszero06@gmail.com>
2024-01-14 16:48:30 +0200

    Update __manifest__.py

--- create_stock_picking_restricted/__manifest__.py
+++ create_stock_picking_restricted/__manifest__.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Zero For Information Systems (<www.erpzero.com>)
+# Copyright(c): 2016-Zero For Information Systems
+# All Rights Reserved.
+#zerosystems #odoo #erp
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+
 {
     'name': 'Create Manual Stock Picking Restricted',
     'version': '6.1.0',
